2805b86fd1a8584970a0c69c286faaa1e7ef39bb
Nirav Patel <patel099688@gmail.com>
2022-09-02 15:39:28 +0530

    icons updated

--- app/src/main/java/com/lma/Adminapp/MainActivity.java
+++ app/src/main/java/com/lma/Adminapp/MainActivity.java
@@ -1,14 +1,28 @@
 package com.lma.Adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                splashScreen.setKeepOnScreenCondition(new SplashScreen.KeepOnScreenCondition() {
+                    @Override
+                    public boolean shouldKeepOnScreen() {
+                        return false;
+                    }
+                });
+            }
+        },3000);
     }
 }
