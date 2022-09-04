2570c27fd9e1f75f2fe4f3c2cfb21b35b290c3bc
Pooran Kunwar <kunwarpooran8@gmail.com>
2022-09-04 16:21:11 +0530

    Fp added

--- app/src/main/java/com/lma/Adminapp/ForgotPassword.java
+++ app/src/main/java/com/lma/Adminapp/ForgotPassword.java
@@ -2,7 +2,12 @@ package com.lma.Adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Forget Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/lma/Adminapp/Login.java
+++ app/src/main/java/com/lma/Adminapp/Login.java
@@ -2,15 +2,18 @@ package com.lma.Adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Objects;
 
 public class Login extends AppCompatActivity {
-
+Button fp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +22,14 @@ public class Login extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
+        fp =findViewById(R.id.forget_btn);
+        fp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Login.this,ForgotPassword.class);
+                startActivity(i);
+            }
+        });
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
