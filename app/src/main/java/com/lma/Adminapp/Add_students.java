90ee3210cba10ae3dffb67dfa36d3d939fb1d627
Jeel Kavathiya <jeelkavathiya@gmail.com>
2022-09-04 21:23:33 +0530

    addstudents

--- app/src/main/java/com/lma/Adminapp/Add_students.java
+++ app/src/main/java/com/lma/Adminapp/Add_students.java
@@ -2,7 +2,12 @@ package com.lma.Adminapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 public class Add_students extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Add_students extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_students);
+        Objects.requireNonNull(getSupportActionBar()).setTitle(" Add Students");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
