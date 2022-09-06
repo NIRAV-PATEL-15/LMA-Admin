2acb0560235a27f8ea20ccea711591a1b6d0af19
Nirav Patel <patel099688@gmail.com>
2022-09-06 22:24:44 +0530

    registration image upload

--- app/src/main/java/com/lma/Adminapp/Registration.java
+++ app/src/main/java/com/lma/Adminapp/Registration.java
@@ -1,23 +1,36 @@
 package com.lma.Adminapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class Registration extends AppCompatActivity {
 
     TextInputLayout fullname_var,username_var,email_var,pass_var,cpass_var,dob_var,phone_var,graduation_var;
     RadioButton rm;
-
+ImageView pp;
+private Bitmap bmp;
+private final int REQ = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +39,7 @@ public class Registration extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
-
+pp = findViewById(R.id.profile);
         fullname_var = findViewById(R.id.fullname_field);
         username_var = findViewById(R.id.username_field);
         email_var = findViewById(R.id.email_field);
@@ -36,7 +49,7 @@ public class Registration extends AppCompatActivity {
         phone_var = findViewById(R.id.phn_field);
         graduation_var = findViewById(R.id.graduation_field);
         rm = findViewById(R.id.rg_male);
-
+registerForContextMenu(pp);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -44,6 +57,45 @@ public class Registration extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.addimage,menu);
+
+    }
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.u_pp:
+                opengallery();
+                return true;
+
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    private void opengallery() {
+        Intent pick_image = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pick_image,REQ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ && resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            try {
+                bmp = MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            pp.setImageBitmap(bmp);
+        }
     }
 
     public void registerbtn(View view) {
