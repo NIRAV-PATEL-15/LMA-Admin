af2bec3c1bbfd1585d77edead9d7f6a34f56a92b
Nirav Patel <patel099688@gmail.com>
2022-09-22 23:43:06 +0530

    manage profile

--- app/src/main/java/com/lma/Adminapp/ManageProfile.java
+++ app/src/main/java/com/lma/Adminapp/ManageProfile.java
@@ -1,15 +1,36 @@
 package com.lma.Adminapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
 public class ManageProfile extends AppCompatActivity {
+    private TextView username,fullname,email,gender,dob,phn,gradu;
+    private Button ep;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference db;
+    private FirebaseAuth mAuth;
+    private  User_Model userModel;
+    private ProgressBar loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,68 @@ public class ManageProfile extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+loading = findViewById(R.id.mp_loading);
+loading.setVisibility(View.VISIBLE);
+        //getting userid from authentication
+
+        String userid = mAuth.getCurrentUser().getUid().toString();
+        ep = findViewById(R.id.get_btn);
+
+        ep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(ManageProfile.this, EditProfile.class);
+                i.putExtra("uid",userid);
+                i.putExtra("data",userModel);
+//                i.putExtra("fname",userModel.getFullname());
+//                i.putExtra("username",userModel.getUsername());
+//                i.putExtra("email",userModel.getEmail());
+//                i.putExtra("gender",userModel.getGender());
+//                i.putExtra("dob",userModel.getDob());
+//                i.putExtra("contact",userModel.getCno());
+//                i.putExtra("graduation",userModel.getGraduation());
+                startActivity(i);
+                finishAndRemoveTask();
+
+
+            }
+        });
+        // Fetching the textviews
+        username = findViewById(R.id.mp_username);
+        fullname = findViewById(R.id.mp_fullname);
+        email = findViewById(R.id.mp_email);
+        gender = findViewById(R.id.mp_gender);
+        dob = findViewById(R.id.mp_dob);
+        phn = findViewById(R.id.mp_contact);
+        gradu = findViewById(R.id.mp_graduation);
+
+        //getting vlaue from realtime database of particular user
+        db = firebaseDatabase.getReference("Teachers").child(userid);
+        //
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                loading.setVisibility(View.GONE);
+                userModel= snapshot.getValue(User_Model.class);
+                username.setText(userModel.getUsername());
+                fullname.setText(userModel.getFullname());
+                email.setText(userModel.getEmail());
+                gender.setText(userModel.getGender());
+                dob.setText(userModel.getDob());
+                phn.setText(userModel.getCno());
+                gradu.setText(userModel.getGraduation());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                loading.setVisibility(View.GONE);
+                Toast.makeText(ManageProfile.this, "error "+error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
