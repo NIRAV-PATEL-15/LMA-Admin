f85bfe962e9f0fafc7845157c1021962cb587c5c
Nirav Patel <patel099688@gmail.com>
2022-10-04 13:24:52 +0530

    student detail

--- app/src/main/java/com/lma/Adminapp/Student_details.java
+++ app/src/main/java/com/lma/Adminapp/Student_details.java
@@ -36,55 +36,36 @@ public class Student_details extends AppCompatActivity {
         setContentView(R.layout.activity_student_details);
 
         //SupportActionBar
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Students Details");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Student Details");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#3E5D7C")));
-        getWindow().setNavigationBarColor(ContextCompat.getColor(this,R.color.bg));
+        getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.bg));
 
 
-//        loading = findViewById(R.id.sd_loading);
-//        loading.setVisibility(View.VISIBLE);
-//
-//        fullname = findViewById(R.id.sd_fullname);
-//        username = findViewById(R.id.sd_username);
-//        email = findViewById(R.id.sd_email);
-//        gender = findViewById(R.id.sd_gender);
-//        dob = findViewById(R.id.sd_dob);
-//        phone = findViewById(R.id.sd_phone);
-//        semester = findViewById(R.id.sd_semester);
-//        division = findViewById(R.id.sd_division);
-//        branch = findViewById(R.id.sd_branch);
+
+        fullname = findViewById(R.id.sd_fullname);
+        username = findViewById(R.id.sd_username);
+        email = findViewById(R.id.sd_email);
+        gender = findViewById(R.id.sd_gender);
+        dob = findViewById(R.id.sd_dob);
+        phone = findViewById(R.id.sd_phone);
+        semester = findViewById(R.id.sd_semester);
+        division = findViewById(R.id.sd_division);
+        branch = findViewById(R.id.sd_branch);
+
+
+        student_model = getIntent().getParcelableExtra("Students");
+        if (student_model != null) {
+            fullname.setText(student_model.getFullname());
+            username.setText(student_model.getUsername());
+            email.setText(student_model.getEmail());
+            gender.setText(student_model.getGender());
+            dob.setText(student_model.getDob());
+            phone.setText(student_model.getPhone());
+            semester.setText(student_model.getSemester());
+            division.setText(student_model.getDivision());
+        }
     }
-//
-//    @Override
-//    protected void onStart() {
-//        firebaseDatabase = FirebaseDatabase.getInstance();
-//        mAuth = FirebaseAuth.getInstance();
-//        String userid = mAuth.getCurrentUser().getUid().toString();
-//        databaseReference = firebaseDatabase.getReference("Students").child(userid);
-//        databaseReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                loading.setVisibility(View.GONE);
-//                student_model = snapshot.getValue(Student_Model.class);
-//                fullname.setText(student_model.getFullname());
-//                username.setText(student_model.getUsername());
-//                email.setText(student_model.getEmail());
-//                gender.setText(student_model.getGender());
-//                dob.setText(student_model.getDob());
-//                phone.setText(student_model.getPhone());
-//                semester.setText(student_model.getSemester());
-//                division.setText(student_model.getDivision());
-//                branch.setText(student_model.getBranch());
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                loading.setVisibility(View.GONE);
-//                Toast.makeText(Student_details.this, "error "+error.toString(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//        super.onStart();
-//    }
+
 }
